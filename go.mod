22090ba2c817e1c785b71eb04bdfd2f2b2b5bf19
Faurby <lasse_fk@hotmail.com>
2022-11-24 20:34:34 +0100

    feat: added a little bit, but it is hard

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module GoMap
 
 go 1.17
+
+require (
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/glaslos/go-osm v0.0.0-20170316165313-16aac6148584 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.1.0 // indirect
+)
--- src/hello.go
+++ src/hello.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/fogleman/gg"
+	"github.com/glaslos/go-osm"
+)
 
 func main() {
-	fmt.Print("Hello, world!")
+	p := filepath.Join("data", "map.osm")
+	m, err := osm.DecodeFile(p)
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println(m.Nodes[0].Lat, m.Nodes[0].Lng)
+
+	dc := gg.NewContext(100, 100)
+	dc.DrawCircle(50, 50, 40)
 }
